842049131ac8d9d496889282f6792a814fe3dbae
KMY <tt@kmycode.net>
2024-12-06 12:50:17 +0900

    Fix test

--- db/post_migrate/20190519130537_remove_boosts_widening_audience.rb
+++ db/post_migrate/20190519130537_remove_boosts_widening_audience.rb
@@ -7,17 +7,17 @@ class RemoveBoostsWideningAudience < ActiveRecord::Migration[5.2]
     # add_column :statuses, :searchability, :integer
     # add_column :statuses, :limited_scope, :integer
 
-    Status.find_by_sql(<<-SQL.squish)
-      SELECT boost.id
-      FROM statuses AS boost
-      LEFT JOIN statuses AS boosted ON boost.reblog_of_id = boosted.id
-      WHERE
-        boost.id > 101746055577600000
-        AND (boost.local = TRUE OR boost.uri IS NULL)
-        AND boost.visibility IN (0, 1)
-        AND boost.reblog_of_id IS NOT NULL
-        AND boosted.visibility = 2
-    SQL
+    # Status.find_by_sql(<<-SQL.squish)
+    #   SELECT boost.id
+    #   FROM statuses AS boost
+    #   LEFT JOIN statuses AS boosted ON boost.reblog_of_id = boosted.id
+    #   WHERE
+    #     boost.id > 101746055577600000
+    #     AND (boost.local = TRUE OR boost.uri IS NULL)
+    #     AND boost.visibility IN (0, 1)
+    #     AND boost.reblog_of_id IS NOT NULL
+    #     AND boosted.visibility = 2
+    # SQL
 
     # Sorry, but remove to fix test
     # RemovalWorker.push_bulk(public_boosts.pluck(:id))
